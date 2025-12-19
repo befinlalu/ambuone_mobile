361bc46fded6f2b29600414fe80fb50ad0e4a115
Befin K Lalu <108138692+befinlalu@users.noreply.github.com>
2025-12-19 12:22:24 +0530

    fix

--- lib/views/register/index.dart
+++ lib/views/register/index.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:ambuone_moblie/models/auth_model/index.dart';
-import 'package:ambuone_moblie/ui_components/text_fields/index.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
